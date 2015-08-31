2f1771a42475d6b9d9e02ab97678fe7873fa03d5
bbosowski6690 <bbosowski6690@outlook.com>
2015-08-30 23:24:11 -0600

    Loaded Examples 1 - 4

---
+++ ExampleSolution4 ConsoleApplicationInputandOutput/ExampleProject4 ConsoleApplicationInputandOutput/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleProject4_ConsoleApplicationInputandOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
