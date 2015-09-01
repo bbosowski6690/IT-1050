9cf7800f6d98a027a0985bb5c214183a7c370cca
bbosowski6690 <bbosowski6690@outlook.com>
2015-08-31 23:09:05 -0600

    Readding Example4

--- ExampleSolution4 ConsoleApplicationInputandOutput/ExampleProject4 ConsoleApplicationInputandOutput/Program.cs
+++ ExampleSolution4 ConsoleApplicationInputandOutput/ExampleProject4 ConsoleApplicationInputandOutput/Program.cs
@@ -10,6 +10,25 @@ namespace ExampleProject4_ConsoleApplicationInputandOutput
     {
         static void Main(string[] args)
         {
+            System.Console.Write("What is your name?");
+
+            string userName;
+
+            userName = System.Console.ReadLine();
+
+            System.Console.Write("How old are you?");
+
+            int userAge;
+            userAge = int.Parse(System.Console.ReadLine());
+
+            System.Console.Write("Your name is :");
+            System.Console.Write(userName);
+            System.Console.Write("Your age is : ");
+            System.Console.WriteLine(userAge);
+
+            System.Console.Write("Press any key to continue...");
+            System.Console.ReadKey();
+             
         }
     }
 }
