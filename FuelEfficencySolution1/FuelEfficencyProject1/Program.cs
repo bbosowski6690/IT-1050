90e6909309cde8a908ebd143e5cf194e7d7a294d
Brittany Bosowski <bbosowski6690@outlook.com>
2015-09-01 20:45:58 -0400

    fUELeFFICENCY

---
+++ FuelEfficencySolution1/FuelEfficencyProject1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace FuelEfficencyProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pricePerGallon;
+            pricePerGallon = 2.35;
+
+            System.Console.WriteLine("Enter Vehicle Year");
+            string vehicleYear = System.Console.ReadLine();
+
+            System.Console.WriteLine("Does the vehicle use deisel?");
+            bool isDeisel = bool.Parse(System.Console.ReadLine());
+
+
+
+
+
+
+
+
+            // Price Per Gallon (Known)
+            
+            // Vehicle Year
+            // Vehicle Make
+            // Vehicle Model
+            // MPG
+            // Gallon Capacity
+            // Current Gallons
+            // Deisel (true or false)
+            
+            // Range (MPG * Number Gallons)
+            // How Much Will It Cost To Fill Up The Tank
+            // How Much Will It Cost To Completely Fill Up The Tank 
+            
+        }
+    }
+}
