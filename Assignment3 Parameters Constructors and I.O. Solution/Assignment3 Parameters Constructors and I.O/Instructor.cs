e359da3eacaf5534b226fdf0525a8d9315853c6e
bbosowski6690 <bbosowski6690@outlook.com>
2015-10-09 23:33:09 -0600

    Assignment 3 Draft 1

---
+++ Assignment3 Parameters Constructors and I.O. Solution/Assignment3 Parameters Constructors and I.O/Instructor.cs
@@ -0,0 +1,36 @@
+﻿namespace Assignment3_Parameters_Constructors_and_I.O
+{
+    class Instructor
+    {
+        public string FirstName;
+        public string LastName;
+        public string CourseName;
+        public SetStudentGrade()
+        {
+            
+        }
+
+      
+        public string GetFullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+
+        public string GetCourseName()
+        {
+            return this.CourseName;
+        }
+ 
+        public void PrintNameandCourse()
+        {
+            System.Console.WriteLine(this.GetFullName = new Name() + this.GetCourseName()); 
+        }
+
+        Create a class “Instructor” according to the following specifications:
+Every Instructor has a Name. (string)
+Every Instructor has a CourseName. (string)
+Every Instructor is created with specified default values for Name and CourseName.
+Every Instructor can SetStudentGrade of any Student. (2 parameters, a Student, and an int)
+Every Instructor can Print their FirstName, LastName and CourseName.
+    }
+}
---
+++ Assignment3 Parameters Constructors and I.O. Solution/Assignment3 Parameters Constructors and I.O/Student.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignment3_Parameters_Constructors_and_I.O
+{
+    class Student
+    {
+        public string Name;
+        public int Grade;
+        public Instructor;
+
+        public void NameGradeInstructor()
+        {
+            Console.WriteLine("Name and Grade: " + this.Name() + this.Grade + "Instructor: "+ this.Instructor());
+        }
+
+
+            Create a class “Student” according to the following specifications:
+Every Student has a Name. (string)
+Every Student has a Grade. (int)
+Every Student has a Teacher. (Instructor)
+Every Student is created with specified default values for Name and Teacher.
+Every Student is created with a default value of 0 for their Grade.
+Every Student can SetGrade which allows their Grade to be assigned a value.
+Every Student can Print their Name, Grade, and Teacher’s complete information.
+
+
+    }
+}
